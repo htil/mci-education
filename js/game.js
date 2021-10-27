d9207e702ff4fbace988d085f5ba1b42e9afb9a8
Chris Crawford <chris35469@gmail.com>
2021-10-27 13:49:44 -0500

    Add phaser example

---
+++ js/game.js
@@ -0,0 +1,17 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
+};
+
+var game = new Phaser.Game(config);
