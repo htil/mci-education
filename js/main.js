543093ecaa36e6998aabf525f0f6c9ecb508d76a
BryanHernandez04 <byhernandez@crimson.ua.edu>
2021-11-11 13:21:26 -0600

    Web bluetooth implemented

--- js/main.js
+++ js/main.js
@@ -17,6 +17,84 @@ var Main = function() {
 
     this.blocklyInterface = new BlocklyInterface()
     this.runButton = document.getElementById('runButton');
+
+    window.Device = new BCIDevice({
+		dataHandler: data => {
+            console.log(data);
+			data.data.forEach(el => {
+				if (buffer.length > BUFFER_SIZE) buffer.shift();
+				buffer.push(el);
+			});
+
+			if (buffer.length < BUFFER_SIZE) return;
+
+			let psd = window.bci.signal.getPSD(BUFFER_SIZE, buffer);
+
+			let alpha = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "alpha");
+			let beta  = window.bci.signal.getBandPower(BUFFER_SIZE,psd, 256, "beta");
+			let theta = window.bci.signal.getBandPower(BUFFER_SIZE,psd, 256, "theta");
+			let gamma = window.bci.signal.getBandPower(BUFFER_SIZE,psd, 256, "gamma");
+			let engagement = beta / (alpha + theta);
+			let sum = alpha + beta + theta + gamma;
+
+			let w_alpha = alpha / sum;
+			let w_beta = beta / sum;
+			let w_theta = theta / sum;
+			let w_gamma = gamma / sum;
+
+			if (weighted.alpha < 0) {
+				weighted.alpha = w_alpha || 0;
+				weighted.beta = w_beta || 0;
+				weighted.theta = w_theta || 0;
+				weighted.gamma = w_gamma || 0;
+				weighted.engagement = engagement || 0;
+			} else {
+				weighted.alpha = weighted.alpha * WEIGHT + (w_alpha || 0) * (1 - WEIGHT);
+				weighted.beta =  weighted.beta  * WEIGHT + (w_beta  || 0)  * (1 - WEIGHT);
+				weighted.theta = weighted.theta * WEIGHT + (w_theta || 0) * (1 - WEIGHT);
+				weighted.gamma = weighted.gamma * WEIGHT + (w_gamma || 0) * (1 - WEIGHT);
+				weighted.engagement = weighted.engagement * WEIGHT + (engagement || 0) * (1 - WEIGHT);
+			}
+
+			// if (window.gameInstance.__ready == true) {
+			// 	// window.gameInstance.SendMessage("Drone", "SetSpeed", (weighted.gamma/weighted.alpha));
+			// 	dataManager.bands = weighted;
+			// 	// console.log((weighted.gamma/weighted.alpha));
+
+			// 	// document.getElementById("pre-start").style.display = "none";
+			// 	// document.getElementById("footer").style.display = "flex";
+			// }
+		},
+		statusHandler: status => {
+			console.log(status)
+		},
+		connectionHandler: connected => {
+			if (!connected) {
+				alert("Device connection lost. Please reconnect.")
+				window.location.reload()
+			}
+		}
+	});
+
+	let connect = async() => {
+		try {
+			await window.Device.connect();
+			// Plot data
+			// window.gameInstance = UnityLoader.instantiate("gameContainer", "Build/bdr-simulator.json", {onProgress: UnityProgress, Module: {
+			// 	onRuntimeInitialized: function () {
+			// 		UnityProgress(gameInstance, "complete");
+			// 		window.gameInstance.__ready = true;
+			// 	},
+			// }});
+		} catch (e) {
+			console.log("connect/load error. retrying...");
+			connect();
+		}
+	}
+
+    $("#connect").on("click", () => {
+		connect();
+	});
    
 
     this.createEventListener = function(id, callback) {
